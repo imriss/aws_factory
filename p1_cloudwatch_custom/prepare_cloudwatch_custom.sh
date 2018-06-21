55aee8ee02303866101f963d28fd738b396edd9a
Reza Farrahi <reza.farrahi@nakisa.com>
2018-06-21 13:41:17 -0400

    More README: Including the repeat of the CloudWatch alarm emails every hour.

--- p1_cloudwatch_custom/prepare_cloudwatch_custom.sh
+++ p1_cloudwatch_custom/prepare_cloudwatch_custom.sh
@@ -4,9 +4,9 @@
 # How to call: */1 * * * * /opt/aws-scripts-mon/mon-put-instance-data.pl ... --cool-service-stats ...
 
 #
-apt install unzip libwww-perl libdatetime-perl jq -y; cd ~; curl -OL http://aws-cloudwatch.s3.amazonaws.com/downloads/CloudWatchMonitoringScripts-1.2.1.zip; unzip CloudWatchMonitoringScripts-1.2.1.zip; mv aws-scripts-mon /opt/; 
+apt install unzip libwww-perl libdatetime-perl jq -y; cd ~; curl -OL http://aws-cloudwatch.s3.amazonaws.com/downloads/CloudWatchMonitoringScripts-1.2.1.zip; unzip CloudWatchMonitoringScripts-1.2.1.zip; mv aws-scripts-mon /opt/;
 
-# 
-cd /opt/aws-scripts-mon/; mv mon-put-instance-data.pl mon-put-instance-data.pl_bckup; curl -O https://raw.githubusercontent.com/imriss/aws_factory/master/mon-put-instance-data.pl; 
+#
+cd /opt/aws-scripts-mon/; mv mon-put-instance-data.pl mon-put-instance-data.pl_bckup; curl -O https://raw.githubusercontent.com/imriss/aws_factory/master/mon-put-instance-data.pl;
 
 # Do not forget to change "cool service" and "awesome container" names to yours.
